62574cdb6e618cb08e8a747a1406bb7d5ce3ddc4
Keyur Shah <keyurshah3939@gmail.com>
2022-11-07 17:41:37 +0530

    Component Path Added

---
+++ packages/nav-bars/NavBar2/src/nav-bar2/nav-bar2.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nav-bars-nav-bars',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bars.component.html',
+  styleUrls: ['./nav-bars.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class NavBar2Component {
+}
