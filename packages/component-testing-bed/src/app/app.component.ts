1fdf1516df8cbfb526b82ebd1991d88e84a3453b
Keyur Shah <keyurshah3939@gmail.com>
2022-11-07 17:34:03 +0530

    TestingBadAdded

---
+++ packages/component-testing-bed/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'rps-angular-components-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'component-testing-bed';
+}
