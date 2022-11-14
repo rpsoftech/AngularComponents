315ff501ba0f024d95c1a3fda4f6b20557c8b323
Keyur Shah <keyurshah3939@gmail.com>
2022-11-14 16:26:33 +0530

    Componet Created

---
+++ packages/nav-bars/src/lib/nav-bar-3/nav-bar3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nav-bars-nav-bar3',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bar3.component.html',
+  styleUrls: ['./nav-bar3.component.scss'],
+})
+export class NavBar3Component {
+}
